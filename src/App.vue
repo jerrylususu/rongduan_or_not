de44756eedbe7f2c061bb76b0dfdcc87987f869d
Neko Null <jerrylu.mail@qq.com>
2022-04-22 09:17:46 +0300

    (misc) add google analytics

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <flight-view></flight-view>
+  
+  <component :is="'script'">
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-WKB857318N');
+  </component>
 </template>
 
 <script>
@@ -12,7 +20,12 @@ export default {
   },
   created () {
       document.title = "今天航班熔断了吗？";
-  }
+  },
+  mounted() {
+    let gaScript = document.createElement('script')
+    gaScript.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=G-WKB857318N')
+    document.head.appendChild(gaScript)
+  },
 }
 </script>
 
